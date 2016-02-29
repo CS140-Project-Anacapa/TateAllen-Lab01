3e6513814f76b421a3e23022f46e60d2353b0245
Tate Allen <tatea7@yahoo.com>
2016-02-28 19:42:28 -0800

    Added testables

---
+++ Makefile
@@ -0,0 +1,8 @@
+CXX=clang++
+DEBUG=-g
+
+testable1:
+	$(CXX) testable1.cpp -o testable1
+
+testable2:
+	$(CXX) testable2.cpp -o testable2
---
+++ testable2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+  if (strcmp(argv[1], "bar")) {
+    std::cout << "" << std::endl;
+  } else if (strcmp(argv[1], "foo")) {
+    std::cout << "Hello, Anacapa!  This is the expected output." << std::endl;
+  }
+
+  return 0;
+}
