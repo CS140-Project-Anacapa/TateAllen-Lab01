1ed344aaa358011694b145103670085acbbebc37
Tate Allen <tatea7@yahoo.com>
2016-02-28 20:20:48 -0800

    test push

--- testable2.cpp
+++ testable2.cpp
@@ -7,7 +7,8 @@ int main(int argc, char const *argv[])
 {
   if (strcmp(argv[1], "bar")) {
     //std::cout << ""; // << std::endl;
-    while (true) {}
+    int i = 0;
+    while (i < 6) { usleep(1E6); }
 
   } else if (strcmp(argv[1], "foo")) {
     std::cout << "Hello, Anacapa!  This is the expected output." << std::endl;
