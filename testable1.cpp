db3ca003a4b907a14739afa73928d606613c6cbe
Tate Allen <tatea7@yahoo.com>
2016-03-02 12:55:01 -0800

    test push

--- testable1.cpp
+++ testable1.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char const *argv[])
 {
   if (strcmp(argv[1], "foo")) {
-    std::cout << "wrong input23413432jkfjkdsl!";
+    std::cout << "";
   } else if (strcmp(argv[1], "bar")) {
     std::cout << "Hello, Anacapa!  This is the expected output." << std::endl;
   }
